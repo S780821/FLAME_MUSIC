3be9873c500118c5ec87e0e7bceb6aed1eb0dc43
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-05-24 06:26:28 +0530

    Add files via upload

--- FlameMusic/converter/cli.py
+++ FlameMusic/converter/cli.py
@@ -1,1 +1,11 @@
+from FlameMusic.config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+from pyrogram import Client
 
+app = Client(
+    "FlameMusic",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+userbot = Client(SESSION_NAME, API_ID, API_HASH)
